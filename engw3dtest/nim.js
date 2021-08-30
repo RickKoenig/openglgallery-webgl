a234a4c5c869a942e061684f1e48e5cae5536727
RickKoenig <mkoeni@sbcglobal.net>
2021-08-29 22:05:25 -0700

    work on nim

--- engw3dtest/nim.js
+++ engw3dtest/nim.js
@@ -4,6 +4,7 @@ var nim = {}; // the nim game state
 nim.text = "Nim Game, a work in progress";
 nim.title = "Nim";
 
+// various states of the game
 nim.states = {
 	intro : 0,
 	humanMove : 1,
@@ -13,33 +14,39 @@ nim.states = {
 	reset : 5,
 	ruleChange : 6
 };
-
-nim.piles = [3,5,7];
-nim.pilesTree;
-nim.currentPiles = [3,5,7];
-
-nim.maxlevel = 1240;
-nim.humanScore = 0;
-nim.computerScore = 0;
 nim.state = nim.states.intro;
 
-nim.counter = 0;
+// game rules
 nim.threeMax = true;
 nim.lastLoses = true;
 
-nim.master = null; // master piece
+// pile info
+nim.piles = [3, 5, 7];
+nim.pilesTree;
+nim.currentPiles = [3, 5, 7];
+nim.pileSpace = [.25, .25];
+nim.pileOffset = [-.25, -.75];
+
+// line turn info
 nim.line = null; // draw a line for move
 
-// load these before init
-nim.load = function() {
-	preloadimg("../common/sptpics/maptestnck.png");
-};
+// overall score
+nim.humanScore = 0;
+nim.computerScore = 0;
 
+// game status
+nim.textInfo = null; // tree font
+
+// test stuff
+nim.maxlevel = 1240;
+nim.counter = 0;
+
+// piles
 nim.createPiles = function() {
 	// build 3d assets
-	nim.master = buildplanexy("aplane",.1,.1,"maptestnck.png","texDoubleSided");
-	nim.master.mod.flags |= modelflagenums.DOUBLESIDED;
-	nim.master.trans = [0,0,1];
+	master = buildplanexy("aplane",.1,.1,"maptestnck.png","texDoubleSided");
+	master.mod.flags |= modelflagenums.DOUBLESIDED|modelflagenums.NOZBUFFER;
+	master.trans = [0,0,1];
 	
 	nim.pilesTree = [];
 	
@@ -47,17 +54,18 @@ nim.createPiles = function() {
 		var column = [];
 		var numCol = nim.piles[i];
 		for (var j = 0; j < numCol; ++j) {
-			var piece = nim.master.newdup();
-			piece.trans[0] = i * .25 - .25;
-			piece.trans[1] = j * .25 - .75;
+			var piece = master.newdup();
+			piece.trans[0] = i * nim.pileSpace[0] + nim.pileOffset[0]; // x
+			piece.trans[1] = j * nim.pileSpace[1] + nim.pileOffset[1]; // y
 			nim.roottree.linkchild(piece);
 			column.push(piece);
 		}
 		nim.pilesTree.push(column);
 	}
+	master.glfree();
 };
 
-nim.drawPiles = function() {
+nim.updatePiles = function() {
 	for (var i = 0; i < nim.piles.length; ++i) {
 		var column = [];
 		var numCol = nim.piles[i];
@@ -72,10 +80,46 @@ nim.drawPiles = function() {
 	}
 };
 
-nim.cleanPiles = function() {
-	nim.master.glfree();
+
+// line for showing turn in progress
+nim.createLine = function() {
+	// line 3d assets
+	nim.line = buildplanexy("line",1,1,"maptestnck.png","texDoubleSided");
+	nim.line.mod.flags |= modelflagenums.DOUBLESIDED|modelflagenums.NOZBUFFER;
+	nim.line.trans = [0,0,1];
+	nim.line.scale = [.025,.1,.1];
+	nim.roottree.linkchild(nim.line);
 };
 
+nim.updateLine = function() {
+	nim.line.trans = [input.fmx, input.fmy, 1];
+};
+
+
+// info about status of game
+nim.createTextInfo = function () {
+	// label fancy scene
+	var ftree = new Tree2("nim game info");
+	var scratchfontmodel = new ModelFont("font for nim","font3.png","tex",
+		1,1,
+		64,8,
+		true);
+	scratchfontmodel.flags |= modelflagenums.DOUBLESIDED|modelflagenums.NOZBUFFER;
+	var str = "Info";
+	scratchfontmodel.print(str);
+	ftree.setmodel(scratchfontmodel);
+	var scaledown = .1;
+	var signOffset = 2.4; // 1.25 is golden!
+	ftree.trans = [-1*scaledown*str.length,signOffset,-signOffset];
+	ftree.scale = [.2,.2,.2];
+	return ftree;
+};
+
+// update instructions for the game
+nim.updateTextInfo = function(text) {
+};
+
+// test stuff
 nim.lesslevel = function() {
 	if (nim.curlevel > 0)
 		--nim.curlevel;
@@ -90,6 +134,13 @@ nim.morelevel = function() {
 
 nim.updatelevel = function() {
 	printareadraw(nim.levelarea,"Level : " + nim.curlevel + "\nhi\nho");
+};
+
+
+// here we go, boot up the state
+// load these before init
+nim.load = function() {
+	preloadimg("../common/sptpics/maptestnck.png");
 };
 
 nim.init = function() {
@@ -124,21 +175,15 @@ nim.init = function() {
 	nim.roottree = new Tree2("nim root tree");
 
 	// build 3d assets
-	// line 3d assets
-	nim.line = buildplanexy("line",1,1,"maptestnck.png","texDoubleSided");
-	nim.line.mod.flags |= modelflagenums.DOUBLESIDED;
-	nim.line.trans = [.5,.5,1];
-	nim.line.scale = [.1,.1,.1];
-	nim.roottree.linkchild(nim.line);
-	// piles 3d assets
 	nim.createPiles();
+	nim.createLine();
 	
 	// viewport
 	mainvp = defaultviewport();	
 	mainvp.clearcolor = [.5,.5,1,1];
 	
 	// test
-	nim.currentPiles = [2,4,6];
+	//nim.currentPiles = [2,4,3];
 };
 
 nim.proc = function() {
@@ -149,17 +194,22 @@ nim.proc = function() {
 		nim.bl.disabled = !nim.bl.disabled;
 		nim.bh.disabled = !nim.bh.disabled;
 		nim.count = 0;
+		/*
 		nim.currentPiles[1]--;
-		if (nim.currentPiles[1] < 0)
+		if (nim.currentPiles[1] < 0) {
 			nim.currentPiles[1] += nim.piles[1] + 1;
+		}
+		*/
 	}
 	
 	// proc line
-	nim.line.trans = [input.fmx, input.fmy, 1];
+	nim.updateLine();
 	
 	// proc graphics
-	nim.drawPiles();
-	nim.roottree.proc(); // probably does nothing
+	nim.updatePiles();
+	
+	// general proc
+	nim.roottree.proc(); // probably does nothing, run animations if available, and user procs too
 	doflycam(mainvp); // modify the trs of mainvp using flycam
 	
 	// draw
@@ -173,7 +223,6 @@ nim.exit = function() {
 	logrc();
 	logger("after roottree glfree\n");
 	nim.roottree.glfree();
-	nim.cleanPiles();
 	
 	// show usage after cleanup
 	logrc();
