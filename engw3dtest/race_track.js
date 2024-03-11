ed6b1d906fdfcc0c02d1cf33aa1350d080c57ca0
RickKoenig <mkoeni@sbcglobal.net>
2024-03-10 20:42:28 -0700

    more work on track collisions, placement after collisions

--- engw3dtest/race_track.js
+++ engw3dtest/race_track.js
@@ -16,7 +16,12 @@ race_track.depth = 4.2;
 race_track.debvars = {
 	testarr:[3,4,[5,7],6],
 	testobj:{"hi":40,"ho":[50,99]},
-    testBool:true
+    testBool:true,
+    testStr:"hi",
+    testNan: NaN,
+    testInf: Infinity,
+    testNull: null,
+    testund: undefined
 };
 
 // test moving object (car)
@@ -110,7 +115,8 @@ race_track.buildtrack = function(trackData) {
 race_track.collideTrack = function(trackData, pos) {
     const trackWidth = trackData[0].length;
     const trackHeight = trackData.length;
-    let fx = pos[0] / 2 + trackWidth  / 2;
+    // piece index and offset
+    let fx = pos[0] / 2 + trackWidth / 2;
     let fy = -pos[1] / 2 + trackHeight / 2;
     const ix = Math.floor(fx);
     const iy = Math.floor(fy);
@@ -118,8 +124,19 @@ race_track.collideTrack = function(trackData, pos) {
     fy -= iy;
     fx = 2 * fx - 1;
     fy = 1 - 2 * fy;
-    let pieceEnum = 0;
-    let pieceRot = 0;
+    // check bounds, make blank if out of bounds
+    let pieceEnum;
+    let pieceRot;
+    if (ix >=0 && ix < trackWidth && iy >= 0 && iy < trackHeight) {
+        const pce = trackData[iy][ix];
+        pieceEnum = pce.type;
+        pieceRot = pce.rot;
+    } else { // center on blank
+        pieceEnum = race_track.trackTypeEnums.blank;
+        pieceRot = 0;
+        fx = fy = 0;
+    }
+
     const collInfo = {
         pieceData: {
             name: race_track.trackTypeStrs[pieceEnum],
@@ -127,33 +144,59 @@ race_track.collideTrack = function(trackData, pos) {
         },
         pieceIdx: [ix, iy],
         pieceOffset: [fx, fy],
-        collide: true
+        collidePos: null,
+        collide: false
     }
-    if (ix >=0 && ix < trackWidth && iy >= 0 && iy < trackHeight) {
-        const pce = trackData[iy][ix];
-        pieceEnum = pce.type;
-        pieceRot = pce.rot;
-        if (pieceEnum != race_track.trackTypeEnums.blank) {
-            const pop = Array(2); // piece offset piece space 
-            race_track.rotateVert(pop, collInfo.pieceOffset, 0, -pieceRot);
-            const border = .5; // between grass and pavement
-            switch(pieceEnum) {
-                case race_track.trackTypeEnums.straight:
-                case race_track.trackTypeEnums.startFinish:
-                    collInfo.collide = pop[1] < -border || pop[1] > border;
-                    break;
-                case race_track.trackTypeEnums.turn:
-                    const popTurn = [pop[0] + 1, pop[1] + 1]; // from -1,-1 lower right corner
-                    const distSq = popTurn[0] * popTurn[0] + popTurn[1] * popTurn[1];
-                    const minTurn = 1 - border;
-                    const maxTurn = 1 + border;
-                    const minTurnSq = minTurn * minTurn;
-                    const maxTurnSq = maxTurn * maxTurn;
-                    collInfo.collide = distSq < minTurnSq || distSq > maxTurnSq;
-                    break;
+    // collide with different piece types
+    const pop = Array(2); // piece offset piece space 
+    race_track.rotateVert(pop, collInfo.pieceOffset, 0, -pieceRot);
+    const border = .5; // between grass and pavement
+    switch(pieceEnum) {
+        case race_track.trackTypeEnums.blank:
+            collInfo.collide = true;
+            collInfo.collidePos = [0, 0];
+            break;
+        case race_track.trackTypeEnums.straight:
+        case race_track.trackTypeEnums.startFinish:
+            if ( pop[1] < -border || pop[1] > border) {
+                pop[1] = range(-border, pop[1], border);
+                collInfo.collide = true;
+                collInfo.collidePos = [pop[0], pop[1]];
             }
-        }
+            break;
+        case race_track.trackTypeEnums.turn:
+            const popTurn = [pop[0] + 1, pop[1] + 1]; // from -1,-1 lower right corner
+            const distSq = popTurn[0] * popTurn[0] + popTurn[1] * popTurn[1];
+            const minTurn = 1 - border;
+            const maxTurn = 1 + border;
+            const minTurnSq = minTurn * minTurn;
+            const maxTurnSq = maxTurn * maxTurn;
+            if (distSq < minTurnSq) {
+                collInfo.collide = true;
+                vec2.Cnormalize(pop, pop);
+                collInfo.collidePos = [pop[0] * minTurn + 1, pop[1] * minTurn - 1];
+            } else if (distSq > maxTurnSq) {
+                collInfo.collide = true;
+                vec2.Cnormalize(popTurn, popTurn);
+                collInfo.collidePos = [popTurn[0] * maxTurn + 1, popTurn[1] * maxTurn - 1];
+            }
+            break;
     }
+    // move collide pos back to track space
+    if (collInfo.collidePos) {
+        race_track.rotateVert(pop, pop, 0, pieceRot);
+        collInfo.collidePos[0] = -trackWidth + 1 + 2 * ix + pop[0];
+        collInfo.collidePos[1] = trackHeight - 1 - 2 * iy + pop[1];
+        /*
+        let fx = pos[0] / 2 + trackWidth / 2;
+        let fy = -pos[1] / 2 + trackHeight / 2;
+        const ix = Math.floor(fx);
+        const iy = Math.floor(fy);
+        fx -= ix;
+        fy -= iy;
+        fx = 2 * fx - 1;
+        fy = 1 - 2 * fy;*/
+        }
     return collInfo;
 }
 
@@ -188,7 +231,8 @@ race_track.buildCar = function() {
     carTree.scale = [1, 1, .1];
     const car = {
         model: {
-            pos: [-2.5, -3, 0]
+            pos: [-2.5, -3, 0],
+            desiredPos: null
         },
         tree: carTree
     }
@@ -197,27 +241,57 @@ race_track.buildCar = function() {
 }
 
 race_track.procCar = function() {
-    // move around
     const step = 1 / 32;
-    if (input.keystate[keycodes.RIGHT])
+    // move around
+    // with mouse
+    if (input.mclick[0]) {
+        race_track.carModel.desiredPos = [
+            input.fmx * race_track.depth,
+            input.fmy * race_track.depth
+        ];
+    }
+    // with keyboard
+    if (input.keystate[keycodes.RIGHT]) {
         race_track.carModel.pos[0] += step;
-    if (input.keystate[keycodes.LEFT])
-    race_track.carModel.pos[0] -= step;
-    if (input.keystate[keycodes.UP])
-    race_track.carModel.pos[1] += step;
+        race_track.carModel.desiredPos = null;
+    }
+    if (input.keystate[keycodes.LEFT]) {
+        race_track.carModel.pos[0] -= step;
+        race_track.carModel.desiredPos = null;
+    }
+    if (input.keystate[keycodes.UP]) {
+        race_track.carModel.pos[1] += step;
+        race_track.carModel.desiredPos = null;
+    }
     if (input.keystate[keycodes.DOWN]) {
         race_track.carModel.pos[1] -= step;
+        race_track.carModel.desiredPos = null;
+    }
+    // mouse, move to desiredPos
+    if (race_track.carModel.desiredPos) {
+        const close2 = step * step * 2;
+        const dist2 = vec2.sqrDist(race_track.carModel.desiredPos, race_track.carModel.pos);
+        if (dist2 < close2) {
+            vec2.copy(race_track.carModel.pos, race_track.carModel.desiredPos);
+            race_track.carModel.desiredPos = null;
+        } else {
+            const delta = vec2.create();
+            vec2.sub(delta, race_track.carModel.desiredPos, race_track.carModel.pos);
+            vec2.normalize(delta, delta);
+            vec2.scale(delta, delta, step);
+            vec2.add(race_track.carModel.pos, race_track.carModel.pos, delta);
+        }
     }
     // don't move out of track area
+    const extra = .25;
     race_track.carModel.pos[0] = range(
-        race_track.trackInfo.minPos[0],
+        race_track.trackInfo.minPos[0] - extra,
         race_track.carModel.pos[0],
-        race_track.trackInfo.maxPos[0]);
+        race_track.trackInfo.maxPos[0] + extra);
     race_track.carModel.pos[1] = range(
-        race_track.trackInfo.minPos[1],
+        race_track.trackInfo.minPos[1] - extra,
         race_track.carModel.pos[1],
-        race_track.trackInfo.maxPos[1]);
-    //race_track.carTree.trans = race_track.carModel.pos;
+        race_track.trackInfo.maxPos[1] + extra);
 }
 
 // load these before init
@@ -288,14 +362,14 @@ race_track.init = function() {
     race_track.trackRoot = track.tree;
     race_track.roottree.linkchild(race_track.trackRoot);
     race_track.trackInfo = track.info;
+    // make info text
+    race_track.infoTree = race_track.buildInfo();
+    race_track.roottree.linkchild(race_track.infoTree);
     // make the car
     const car = race_track.buildCar(race_track.roottree);
     race_track.carModel = car.model;
     race_track.carTree = car.tree;
     race_track.roottree.linkchild(car.tree);
-    // make info text
-    race_track.infoTree = race_track.buildInfo();
-    race_track.roottree.linkchild(race_track.infoTree);
         
     // camera viewport
 	mainvp = defaultviewport();	
@@ -309,6 +383,10 @@ race_track.proc = function() {
 	// proc
     race_track.procCar();
     const collInfo = race_track.collideTrack(race_track.trackAData ,race_track.carModel.pos);
+    if (collInfo.collide) {
+        race_track.carModel.pos[0] = collInfo.collidePos[0];
+        race_track.carModel.pos[1] = collInfo.collidePos[1];
+    }
     race_track.carTree.mat.color = collInfo.collide
         ? [1, 0, 0, 1] 
         : [1, 1, 1, 1];
